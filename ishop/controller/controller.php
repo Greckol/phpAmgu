e1b9cebe84bec4cf849da0327c4ea57850f9385d
Greck <vitaliachal@gmail.com>
2024-03-27 14:36:19 +0900

    4 lab

--- ishop/controller/controller.php
+++ ishop/controller/controller.php
@@ -3,4 +3,7 @@ defined('ISHOP') or die('Access denied');
 
 require_once MODEL;
 
+// получение динамичной части шаблона #content
+$view = empty($_GET['view']) ? 'hits' : $_GET['view'];
+
 require_once TEMPLATE.'index.php';
